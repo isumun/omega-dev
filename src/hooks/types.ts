50df45ae9f9c616f20a1a9432987ad306e4af4e4
seyitkulov <islamsejtkulov@gmail.com>
2024-07-02 19:02:10 +0600

    vacancia stil ozgorttym api menen ishtep byttym

---
+++ src/hooks/types.ts
@@ -0,0 +1,19 @@
+export interface Job {
+    id: string;
+    organization_name: string;  
+    organization_icon: string;  
+    position: string;
+    salary: string;
+    type: string;
+    city: string;
+    created_at: string;
+    updated_at: string;
+    currency: string;
+    gradient: number;
+    is_archived: boolean | null;
+    organization_icon_formats: { type: string; url: string }[];
+    price_from: number;
+    price_to: number;
+    slug: string;
+    workday: string | null;
+  }
